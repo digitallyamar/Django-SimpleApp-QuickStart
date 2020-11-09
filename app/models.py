8f132d6ae69d65b4ab7e962bf4184d0bd65e845f
digitallyamar <digitallyamar@gmail.com>
2020-11-09 10:14:19 +0530

    Updated to make Model data available in Admin panel for edit.

--- app/models.py
+++ app/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from crum import get_current_user
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,blank=False)
+
+    def save(self, *args, **kwargs):
+        created_by = get_current_user()
+        super(Todo, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
